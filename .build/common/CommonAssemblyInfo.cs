e87435414def6dc8c22aacd717aecb047a5e1f2d
Andreas Gladik <andreas.gladik@gmail.com>
2016-07-08 10:05:01 +0200

    change version number and clean up

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,7 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
-[assembly: AssemblyInformationalVersion("2.2.0")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright 2016")]
--- Bridge.SignalR/Properties/AssemblyInfo.cs
+++ Bridge.SignalR/Properties/AssemblyInfo.cs
@@ -1,35 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Bridge.SignalR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SignalR C#-wrapper for Bridge.NET projects")]
 [assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bridge.SignalR")]
-//[assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("65e4eac1-c7e7-4973-98cf-0edd45930ba7")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-//[assembly: AssemblyVersion("1.14")]
-//[assembly: AssemblyFileVersion("1.14.0")]
-//[assembly: AssemblyInformationalVersion("1.14.0")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0.0-beta")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta")]
